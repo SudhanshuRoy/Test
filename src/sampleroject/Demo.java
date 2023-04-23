f882be0dd46fe87b67e7817b4d41d690bdd1bb2c
Sudhanshu Kumar <105262161+SudhanshuRoy@users.noreply.github.com>
2023-04-23 23:59:48 +0530

    updating Demo class

--- src/sampleroject/Demo.java
+++ src/sampleroject/Demo.java
@@ -4,9 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Jay Shree Ram !");
 		System.out.println("Hare krishna Hare Ram !");
-		System.out.println("My name is Sudhanshu Roy !");
-		System.out.println("Jay Shree Ram !");
-		System.out.println("Hare krishna Hare Ram !");
+		
 
 	}
 
