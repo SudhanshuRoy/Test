7b261504e81c9f3658de8f0da900e6ffe4d4c865
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2023-04-24 00:01:32 +0530

    add company name

--- src/sampleroject/Demo.java
+++ src/sampleroject/Demo.java
@@ -5,8 +5,7 @@ public class Demo {
 		System.out.println("Jay Shree Ram !");
 		System.out.println("Hare krishna Hare Ram !");
 		System.out.println("My name is Sudhanshu Roy !");
-		System.out.println("Jay Shree Ram !");
-		System.out.println("Hare krishna Hare Ram !");
+		System.out.println("I am working in salesken");
 
 	}
 
