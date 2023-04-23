fca42e0ee9ac68aecfa0712135346b793beb4779
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2023-04-23 23:43:58 +0530

    sys added

--- src/sampleroject/Demo.java
+++ src/sampleroject/Demo.java
@@ -3,6 +3,7 @@ package sampleroject;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Jay Shree Ram !");
+		System.out.println("Hare krishna Hare Ram !");
 	}
 
 }
