321717463d4a06ff95bdc60e097bbd83d19c67f5
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2023-04-26 23:58:39 +0530

    changed in main branch

--- src/sampleroject/Demo.java
+++ src/sampleroject/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Jay Shree Ram !");
 		System.out.println("Hare krishna Hare Ram !");
-		
+		System.out.println("Radhe Radhe !");
 		
 	}
 
--- src/sampleroject/Main.java
+++ src/sampleroject/Main.java
@@ -3,6 +3,7 @@ package sampleroject;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+		System.out.println("Ha.. Ha..");
 	}
 
 }
