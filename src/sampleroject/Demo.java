a8abce8f0a8437bec5225403051141cbb377f0b8
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2023-04-26 23:39:25 +0530

    adding Main class

--- src/sampleroject/Demo.java
+++ src/sampleroject/Demo.java
@@ -4,15 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Jay Shree Ram !");
 		System.out.println("Hare krishna Hare Ram !");
-		System.out.println("My name is Sudhanshu Roy !");
-		System.out.println("I am working in salesken");
-		System.out.println("My native place is  Muzaffarpur !");
-		System.out.println("I am currently living in Banglore !");
-		System.out.println("I am krishna Bhakt !");	
-		System.out.println("I am working in salesken !");
-		System.out.println("I am betch graduate !");
-		System.out.println("How are you ?");
-		System.out.println("Good Morning");
+		
 		
 	}
 
